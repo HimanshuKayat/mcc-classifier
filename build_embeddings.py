358a757b37a8ea49ebd79e881f22955d0ee1d16e
HimanshuKayat <himanshukayat7@gmail.com>
2026-08-07 16:31:10 +0530

    Update build_embeddings.py

--- build_embeddings.py
+++ build_embeddings.py
@@ -3,7 +3,7 @@ import pickle
 
 from sentence_transformers import SentenceTransformer
 
-MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+MODEL_NAME = "BAAI/bge-large-en-v1.5"
 
 
 def profile_to_text(profile):
